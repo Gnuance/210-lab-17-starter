2f4c3977df35045341cc66a9c0bc193a9f98bd97
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-10-04 00:02:51 -0700

    10 min timestamp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,41 @@
+/* Lab 17: Refactor linked list to modularize code into functions.
+    IDE: Vscode
+*/
+
 #include <iostream>
 using namespace std;
 
-const int SIZE = 7;  
-
-struct Node {
+// Linked list node struct
+struct Node
+{
     float value;
     Node *next;
 };
 
 void output(Node *);
+void deleteList(Node *);
 
-int main() {
+int main()
+{
     Node *head = nullptr;
     int count = 0;
+    const int SIZE = 7; // Changed from global variable to a local variable in main
 
     // create a linked list of size SIZE with random numbers 0-99
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < SIZE; i++)
+    {
         int tmp_val = rand() % 100;
         Node *newVal = new Node;
-        
+
         // adds node at head
-        if (!head) { // if this is the first node, it's the new head
+        if (!head)
+        { // if this is the first node, it's the new head
             head = newVal;
             newVal->next = nullptr;
             newVal->value = tmp_val;
         }
-        else { // its a second or subsequent node; place at the head
+        else
+        { // its a second or subsequent node; place at the head
             newVal->next = head;
             newVal->value = tmp_val;
             head = newVal;
@@ -34,7 +44,7 @@ int main() {
     output(head);
 
     // deleting a node
-    Node * current = head;
+    Node *current = head;
     cout << "Which node to delete? " << endl;
     output(head);
     int entry;
@@ -44,15 +54,17 @@ int main() {
     // traverse that many times and delete that node
     current = head;
     Node *prev = head;
-    for (int i = 0; i < (entry-1); i++)
+    for (int i = 0; i < (entry - 1); i++)
         if (i == 0)
             current = current->next;
-        else {
+        else
+        {
             current = current->next;
             prev = prev->next;
         }
     // at this point, delete current and reroute pointers
-    if (current) {  // checks for current to be valid before deleting the node
+    if (current)
+    { // checks for current to be valid before deleting the node
         prev->next = current->next;
         delete current;
         current = nullptr;
@@ -63,7 +75,8 @@ int main() {
     current = head;
     cout << "After which node to insert 10000? " << endl;
     count = 1;
-    while (current) {
+    while (current)
+    {
         cout << "[" << count++ << "] " << current->value << endl;
         current = current->next;
     }
@@ -75,12 +88,13 @@ int main() {
     for (int i = 0; i < (entry); i++)
         if (i == 0)
             current = current->next;
-        else {
+        else
+        {
             current = current->next;
             prev = prev->next;
         }
-    //at this point, insert a node between prev and current
-    Node * newnode = new Node;
+    // at this point, insert a node between prev and current
+    Node *newnode = new Node;
     newnode->value = 10000;
     newnode->next = current;
     prev->next = newnode;
@@ -88,7 +102,8 @@ int main() {
 
     // deleting the linked list
     current = head;
-    while (current) {
+    while (current)
+    {
         head = current->next;
         delete current;
         current = head;
@@ -99,14 +114,31 @@ int main() {
     return 0;
 }
 
-void output(Node * hd) {
-    if (!hd) {
+// deleting the linked list
+void deleteList(Node *hd)
+{
+    current = head;
+    while (current)
+    {
+        head = current->next;
+        delete current;
+        current = head;
+    }
+    head = nullptr;
+}
+
+// Outputs linked list starting at head element
+void output(Node *hd)
+{
+    if (!hd)
+    {
         cout << "Empty list.\n";
         return;
     }
     int count = 1;
-    Node * current = hd;
-    while (current) {
+    Node *current = hd;
+    while (current)
+    {
         cout << "[" << count++ << "] " << current->value << endl;
         current = current->next;
     }
